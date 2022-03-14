59ea6e91a0c3f78a010e1b184a2876f5c492f253
EmreD <EmreD@DESKTOP-7E2N3SF>
2022-03-14 19:11:09 +0300

    Proje dosyası ekle.

---
+++ Riddle Teller/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Riddle_Teller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Titlename = new System.Windows.Forms.Label();
+            this.Riddlebutton = new System.Windows.Forms.Button();
+            this.answerbutton = new System.Windows.Forms.Button();
+            this.quitbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Titlename
+            // 
+            this.Titlename.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Titlename.Font = new System.Drawing.Font("Ravie", 24.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titlename.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Titlename.Location = new System.Drawing.Point(13, 15);
+            this.Titlename.Name = "Titlename";
+            this.Titlename.Size = new System.Drawing.Size(348, 135);
+            this.Titlename.TabIndex = 0;
+            this.Titlename.Text = "Riddle me this";
+            this.Titlename.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Riddlebutton
+            // 
+            this.Riddlebutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Riddlebutton.Font = new System.Drawing.Font("Jokerman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Riddlebutton.Location = new System.Drawing.Point(12, 166);
+            this.Riddlebutton.Name = "Riddlebutton";
+            this.Riddlebutton.Size = new System.Drawing.Size(349, 62);
+            this.Riddlebutton.TabIndex = 1;
+            this.Riddlebutton.Text = "Riddle";
+            this.Riddlebutton.UseVisualStyleBackColor = false;
+            this.Riddlebutton.Click += new System.EventHandler(this.Riddlebutton_Click);
+            // 
+            // answerbutton
+            // 
+            this.answerbutton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.answerbutton.Font = new System.Drawing.Font("Jokerman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerbutton.Location = new System.Drawing.Point(12, 234);
+            this.answerbutton.Name = "answerbutton";
+            this.answerbutton.Size = new System.Drawing.Size(349, 62);
+            this.answerbutton.TabIndex = 2;
+            this.answerbutton.Text = "Answer";
+            this.answerbutton.UseVisualStyleBackColor = false;
+            this.answerbutton.Click += new System.EventHandler(this.answerbutton_Click);
+            // 
+            // quitbutton
+            // 
+            this.quitbutton.BackColor = System.Drawing.Color.Maroon;
+            this.quitbutton.Font = new System.Drawing.Font("Jokerman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quitbutton.Location = new System.Drawing.Point(12, 302);
+            this.quitbutton.Name = "quitbutton";
+            this.quitbutton.Size = new System.Drawing.Size(349, 62);
+            this.quitbutton.TabIndex = 3;
+            this.quitbutton.Text = "Quit";
+            this.quitbutton.UseVisualStyleBackColor = false;
+            this.quitbutton.Click += new System.EventHandler(this.quitbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(373, 378);
+            this.Controls.Add(this.quitbutton);
+            this.Controls.Add(this.answerbutton);
+            this.Controls.Add(this.Riddlebutton);
+            this.Controls.Add(this.Titlename);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Titlename;
+        private System.Windows.Forms.Button Riddlebutton;
+        private System.Windows.Forms.Button answerbutton;
+        private System.Windows.Forms.Button quitbutton;
+    }
+}
+
---
+++ Riddle Teller/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Riddle_Teller
+{
+    public partial class Form1 : Form
+    {
+        int randomnumber;
+        bool pressed = false;
+        int count = 0;
+        Random random = new Random();
+        ArrayList riddlelist = new ArrayList();
+        ArrayList answerlist = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Riddles()
+        {
+            riddlelist.Add("The more you take, the more you leave behind.What am I ?");
+            riddlelist.Add("What 8 letter word can have a letter taken away and it still makes a word. Take another letter away and it still makes a word. Keep on doing that until you have one letter left. What is the word?");
+            riddlelist.Add("What has a head, a tail, is brown, and has no legs?");
+            riddlelist.Add("David's father has three sons: Snap, Crackle, and _____?");
+            riddlelist.Add("A doctor and a bus driver are both in love with the same woman, an attractive girl named Sarah. The bus driver had to go on a long bus trip that would last a week. Before he left, he gave Sarah seven apples. Why?");
+            riddlelist.Add("A boy was at a carnival and went to a booth where a man said to the boy, \"If I write your exact weight on this piece of paper then you have to give me $50, but if I cannot, I will pay you $50.\" The boy looked around and saw no scale so he agrees, thinking no matter what the carny writes he'll just say he weighs more or less. In the end the boy ended up paying the man $50. How did the man win the bet?");
+            riddlelist.Add("What comes once in a minute, twice in a moment, but never in a thousand years?");
+            riddlelist.Add("What has many keys, but can't even open a single door?");
+            riddlelist.Add("What has six faces, but does not wear makeup, has twenty-one eyes, but cannot see? What is it?");
+            riddlelist.Add("wo fathers and two sons went fishing one day. They were there the whole day and only caught 3 fish. One father said, that is enough for all of us, we will have one each. How can this be possible?");
+
+        }
+        private void Answers()
+        {
+            answerlist.Add("Footsteps.");
+            answerlist.Add("The word is starting! starting, staring, string, sting, sing, sin, in, I.  Cool,huh?");
+            answerlist.Add("A Penny.");
+            answerlist.Add("David.");
+            answerlist.Add("An apple a day keeps the doctor away!");
+            answerlist.Add("The man did exactly as he said he would and wrote \"your exact weight\" on the paper.");
+            answerlist.Add("The letter \"m\".");
+            answerlist.Add("What has many keys, but can't even open a single door?");
+            answerlist.Add("A die (dice).");
+            answerlist.Add("There was the father, his son, and his son's son. This equals 2 fathers and 2 sons for a total of 3!");
+
+        }
+
+        private void Riddlebutton_Click(object sender, EventArgs e)
+        {
+            pressed = true;
+            Titlename.Text = "";
+            if (count == 0)
+            {
+                Riddles();
+                Answers();
+                count++;
+            }
+            randomnumber = random.Next(10);
+            Titlename.Font = new Font("Times New Roman",10, FontStyle.Bold);
+            Titlename.Text = Convert.ToString(riddlelist[randomnumber]);
+        }
+
+        private void answerbutton_Click(object sender, EventArgs e)
+        {
+            if (pressed)
+            {
+                Titlename.Font = new Font("Times New Roman", 10, FontStyle.Bold);
+                Titlename.Text = Convert.ToString(answerlist[randomnumber]);
+                pressed = false;
+            }
+            else
+            {
+                Titlename.Font = new Font("Times New Roman", 10, FontStyle.Bold);
+                Titlename.Text = "Press to riddle first!";
+            }
+        }
+
+        private void quitbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
